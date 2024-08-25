9b90c87c719e560a3553ba18a0a7b99489bc54f1
Karthik kotekar <152687159+karthiks779@users.noreply.github.com>
2024-08-25 22:17:27 +0530

    capitalize.js

--- Capitalize.js
+++ Capitalize.js
@@ -1,17 +1,20 @@
-function capitalizeWords(str)
-{ 
+function capitalise()
+ {
+  
+  var string;
+  
+  string =  document.getElementById("second").value;
+  
 
      let res=''
    let gh=''
-       str=str.split(' ')
-      for (let i = 0; i < str.length; i++) {
+       str=string.split(' ')
+      for (let i = 0; i <str.length; i++) {
 
 
 
    res=str[i]
    gh+=res.charAt(0).toUpperCase()+res.slice(1)+" "
        
-      }
-return gh
- }
- console.log(capitalizeWords('js string '))
+      } document.getElementById("answer").value =gh;
+}
